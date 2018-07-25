c2857ddfe66c4601d75c41173f5d7c587eb13d60
Kaveh Saleminejad <lcaveh@gmail.com>
2018-07-25 19:59:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var kaveh = "Joe";
+var nam = "Kaveh";
 var height = 74
 
 console.log("Name:")
-console.log(name)
+console.log(nam)
 
 console.log("Height:")
 console.log(height+1)
